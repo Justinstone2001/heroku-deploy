95f16d4b161ed008e572057f79347931d47907c8
Justin Stone <justone@davidson.edu>
2022-07-27 12:48:52 -0400

    added alt tag to my avatar img

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -6,7 +6,7 @@ export default function AboutMe() {
     return (
         <> 
         <div className='aboutDiv' id='about'>
-            <img src='https://avataaars.io/?avatarStyle=Circle&topType=ShortHairDreads01&accessoriesType=Blank&hairColor=BrownDark&facialHairType=MoustacheFancy&facialHairColor=BrownDark&clotheType=Hoodie&clotheColor=PastelOrange&eyeType=Default&eyebrowType=UpDown&mouthType=Disbelief&skinColor=Light'/>
+            <img alt='avatar' src='https://avataaars.io/?avatarStyle=Circle&topType=ShortHairDreads01&accessoriesType=Blank&hairColor=BrownDark&facialHairType=MoustacheFancy&facialHairColor=BrownDark&clotheType=Hoodie&clotheColor=PastelOrange&eyeType=Default&eyebrowType=UpDown&mouthType=Disbelief&skinColor=Light'/>
             <h2 className='aboutTitle'>About Me: </h2>
             <p className='aboutTxt'> Welcome to my portfolio! I am a current senior at Davidson college studying computer science and Hispanic studies. 
             I have worked in computer science for 2 years of college courses and am now learning full stack web development 
