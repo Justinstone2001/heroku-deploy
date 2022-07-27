772a8b14f993596c3158e84522b42f508edb99f2
Justin Stone <justone@davidson.edu>
2022-07-27 12:47:36 -0400

    updated github and linkedin logos and added avatar to my portfolio

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,12 +4,10 @@ import '../styles/Footer.css';
 
 function Footer() {
   return (
-    <header className="footer">
-      <div  id="linkContainer">
-        <a href="https://www.linkedin.com/in/justin-stone-4071761ab/" className='links'>LinkedIn</a>
-        <a href="https://github.com/Justinstone2001" className='links'>Github</a>
-      </div>
-    </header>
+    <div  id="linkContainer">
+      <a href="https://github.com/Justinstone2001"> <img src="https://img.icons8.com/fluent/48/000000/github.png" alt="GitHub" className="Icon"/></a>
+      <a href="https://www.linkedin.com/in/justin-stone-4071761ab/"> <img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="LinkedIn" className="Icon"/></a>
+    </div>
   );
 }
 
--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -5,7 +5,8 @@ import '../../styles/Content.css';
 export default function AboutMe() {
     return (
         <> 
-        <div className='aboutDiv'>
+        <div className='aboutDiv' id='about'>
+            <img src='https://avataaars.io/?avatarStyle=Circle&topType=ShortHairDreads01&accessoriesType=Blank&hairColor=BrownDark&facialHairType=MoustacheFancy&facialHairColor=BrownDark&clotheType=Hoodie&clotheColor=PastelOrange&eyeType=Default&eyebrowType=UpDown&mouthType=Disbelief&skinColor=Light'/>
             <h2 className='aboutTitle'>About Me: </h2>
             <p className='aboutTxt'> Welcome to my portfolio! I am a current senior at Davidson college studying computer science and Hispanic studies. 
             I have worked in computer science for 2 years of college courses and am now learning full stack web development 
